2eae5c5e05c19e8b1538bc01823237d065f8b5cb
Maksym Liannoi <liannoi444@gmail.com>
2021-04-14 07:44:43 +0300

    Initial commit

---
+++ src/Presentation/Console/Console.Application/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LifetimeCollection.Presentation.Console.Application
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
